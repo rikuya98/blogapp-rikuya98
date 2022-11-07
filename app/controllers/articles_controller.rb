f3985aa22986e17e52f9406746528a02af4f5ac1
日下陸矢 <rikuya.98@gmail.com>
2022-11-07 19:21:13 +0900

    削除機能を追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -32,7 +32,13 @@ class ArticlesController < ApplicationController
         else
             flash.now[:error] = '更新できませんでした'
             render :edit
+        end
     end
+
+    def destroy
+        article = Article.find(params[:id])
+        article.destroy!
+        redirect_to root_path, notice: '記事が削除されました'
     end
 
     private
