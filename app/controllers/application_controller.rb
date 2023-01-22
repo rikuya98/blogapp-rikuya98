8e5aef9587318a0cf40e43623ecc49f37c9fda47
日下陸矢 <rikuya.98@gmail.com>
2023-01-22 18:28:40 +0900

    ・多言語化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,24 @@
 class ApplicationController < ActionController::Base
-    def current_user
-      ActiveDecorator::Decorator.instance.decorate(super) if super.present?
-      super
-    end
+  before_action :set_locale
+  def current_user
+    ActiveDecorator::Decorator.instance.decorate(super) if super.present?
+    super
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  private
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
+# パラメーターの説明
+# /articles/1 => params[:id]
+# /articles?id=1 => params[:id]
+# /articles?user_status=now => params[:user_stauts] 
+# という形で各々取得できる。これはrailsのルール 
+# ?を書いてパラメーターをつける時はroutesに書かなくても自由に設定できる。　気軽にパラメーターを追加したいときは使う。
+# 一般的にはトラッキングコードに使われたりする。マーケティングのツールでどこからアクセスしたのか。キャンペーンでツイッター広告から流入しました
+# というケースで使う。 ?imporession=twitter
