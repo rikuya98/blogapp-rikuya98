9291d9d49813b0e1391e8f0424040823051a1040
日下陸矢 <rikuya.98@gmail.com>
2023-01-23 11:17:53 +0900

    rubocopの実行

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,8 @@ end
 # パラメーターの説明
 # /articles/1 => params[:id]
 # /articles?id=1 => params[:id]
-# /articles?user_status=now => params[:user_stauts] 
-# という形で各々取得できる。これはrailsのルール 
+# /articles?user_status=now => params[:user_stauts]
+# という形で各々取得できる。これはrailsのルール
 # ?を書いてパラメーターをつける時はroutesに書かなくても自由に設定できる。　気軽にパラメーターを追加したいときは使う。
 # 一般的にはトラッキングコードに使われたりする。マーケティングのツールでどこからアクセスしたのか。キャンペーンでツイッター広告から流入しました
 # というケースで使う。 ?imporession=twitter
--- app/models/article.rb
+++ app/models/article.rb
@@ -22,11 +22,8 @@ class Article < ApplicationRecord
 
     validates :content, presence: true
 
-
     has_many :comments, dependent: :destroy
     has_many :likes, dependent: :destroy
     belongs_to :user
-
-
 
 end
